5ac9874b6bd1163647abc03428a66857bf74d889
abekazumasa <kazumasa.danmaro@gmail.com>
2021-10-06 11:50:41 +0900

    [update] update MergePice.cs
    スクリプトを変更しました

--- Assets/Samples/Sample Scene/Script/MergePice.cs
+++ Assets/Samples/Sample Scene/Script/MergePice.cs
@@ -1,35 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+
+//問題のスクリプト
 public class MergePice : MonoBehaviour
 {
-
+    //一つのピース
     [SerializeField]
-    private GameObject CubeA;
-
+    private GameObject pisceA;
+    //もう一つのピース
     [SerializeField]
-    private GameObject CubeB;
-
+    private GameObject pisceB;
+    //ピースの初期位置
     Vector3 offset;
 
-    // Start is called before the first frame update
+    //firstframe
     void Start()
     {
-        offset = CubeB.transform.position - CubeA.transform.position;
+        //初期位置を設定
+        offset = pisceB.transform.position - pisceA.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //今回はボタンを押したら移動するようにしています
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            var translate = CubeA.transform.position;
-            var quateernin = CubeA.transform.rotation;
-            var scale = CubeA.transform.localScale;
-            var matrixs = Matrix4x4.TRS(translate,quateernin,scale);
-            CubeB.transform.position = matrixs.MultiplyPoint(CubeB.transform.position);
-            CubeB.transform.rotation = CubeA.transform.rotation;
+            //平行移動
+            var translate = (pisceA.transform.position - pisceB.transform.position) + offset;
+            //回転
+            var quaternion =pisceA.transform.rotation;
+            //スケール
+            var scale = pisceB.transform.localScale;
+            //アフィン変換行列の設定
+            var matrixs = Matrix4x4.TRS(translate, quaternion, scale);
+            //位置変換
+            pisceB.transform.position = matrixs.MultiplyPoint(pisceB.transform.position);
+            //同じ向きにする
+            pisceB.transform.rotation = pisceA.transform.rotation;
         }
     }
 }
